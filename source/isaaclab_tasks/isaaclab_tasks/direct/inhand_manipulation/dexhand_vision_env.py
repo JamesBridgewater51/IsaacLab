1. Fix bugs of using ground-truth relative quaternion in _compute_image_observations function in dexhand_vision_env.py. 2. Set save_data_to_file->False, train->False, load_checkpoint->True in DexHandVisionEnvCfg instantiation.

source/isaaclab_tasks/isaaclab_tasks/direct/inhand_manipulation/dexhand_vision_env.py
@@ -132,7 +132,7 @@ class DexHandVisionEnvCfg(DexHandEnvCfg):
         width=120,
         height=120,
     )
-    feature_extractor = FeatureExtractorCfg(train=True, save_data_to_file=True, load_checkpoint=False, input_modality="rgb_only", base_dir=os.path.join(os.path.dirname(os.path.dirname(__file__)), "o12_hand", CURRENT_TIME))
+    feature_extractor = FeatureExtractorCfg(train=False, save_data_to_file=False, load_checkpoint=True, input_modality="rgb_only", base_dir=os.path.join(os.path.dirname(os.path.dirname(__file__)), "o12_hand", CURRENT_TIME))
     # feature_extractor = FeatureExtractorCfg(train=True, load_checkpoint=True, input_modality="rgb_only", base_dir=os.path.join(os.path.dirname(os.path.dirname(__file__)), "o12_hand", CURRENT_TIME))
 
 
@@ -255,7 +255,10 @@ class DexHandVisionEnv(InHandManipulationRealEnv):
         compute_keypoints(
             pose=torch.cat((torch.zeros_like(self.goal_pos), self.goal_rot), dim=1), size=size, out=self.goal_keypoints
         )
-        rel_quat = keypoints_to_relquat(self.gt_keypoints, self.goal_keypoints, self.object_pos)  # [B,4]
+        # Ground-truth rel_quat
+        # rel_quat = keypoints_to_relquat(self.gt_keypoints, self.goal_keypoints, self.object_pos)  # [B,4]
+
+        rel_quat = keypoints_to_relquat(self.embeddings[:, 3:].reshape(-1, 8, 3), self.goal_keypoints, self.embeddings[:,:3])  # [B,4]
 
         # 6) Logging
         if "log" not in self.extras: